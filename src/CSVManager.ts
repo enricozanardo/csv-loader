dc25a1610b5a7c81125825c713dd56c93458a4dc
enrico <ezanardo@onezerobinary.com>
2020-05-20 11:03:45 +0200

    add converters to csv-loader class

---
+++ src/CSVManager.ts
@@ -0,0 +1,104 @@
+import fs from 'fs';
+import os from 'os';
+import _ from 'lodash';
+import shuffleSeed from 'shuffle-seed';
+
+export type Value = string | number;
+type Callback = (value: Value) => Value;
+type Converters = { [key: string]: Callback };
+
+export type Options = {
+  dataColumns: string[];
+  labelColumns: string[];
+  converters: Converters;
+  shuffle: boolean;
+  splitTest: 0.0 | 0.1 | 0.2 | 0.3 | 0.4 | 0.5 | 0.6 | 0.7 | 0.8 | 0.9 | 1.0;
+};
+
+export type DataModel = Value[][];
+type Headers = string[number];
+
+export type DataSet = {
+  features: DataModel;
+  labels: DataModel;
+  testFeatures: DataModel;
+  testLabels: DataModel;
+};
+
+export class CSVManager {
+  constructor(public filename: string, public options: Options) {}
+
+  public loadCSV() {
+    const PATH_CSV = `${__dirname}/csv`;
+
+    let loadedData = fs.readFileSync(`${PATH_CSV}/${this.filename}`, {
+      encoding: 'utf-8',
+    });
+    let rawData = _.map(loadedData.split('\n'), (d) => d.split(','));
+    rawData = _.dropRightWhile(rawData, (val) => _.isEqual(val, ['']));
+
+    const headers = _.first(rawData) || [];
+
+    const new_data = _.map(rawData, (row: Value[], index: number) => {
+      if (index == 0) {
+        return row;
+      }
+
+      return _.map(row, (element: Value, index: number) => {
+        //TODO: based on the column name, activate the converter function
+        // that will update the value in that column name, in the given row.
+        if (this.options.converters[headers[index]]) {
+          const converted = this.options.converters[headers[index]](element);
+          // console.log(converted);
+        }
+
+        if (typeof element !== 'number') {
+          element = parseFloat(element.replace('"', ''));
+        }
+
+        return element;
+      });
+    });
+
+    let labels: DataModel = this.extractColumns(
+      new_data,
+      this.options.labelColumns
+    );
+    let extractedData: DataModel = this.extractColumns(
+      new_data,
+      this.options.dataColumns
+    );
+
+    extractedData.shift();
+    labels.shift();
+
+    if (this.options.shuffle) {
+      extractedData = shuffleSeed.shuffle(extractedData, 'enrynewton');
+      labels = shuffleSeed.shuffle(labels, 'enrynewton');
+    }
+
+    const trainSize = Math.floor(extractedData.length * this.options.splitTest);
+
+    const dataSet: DataSet = {
+      features: extractedData.slice(trainSize),
+      labels: labels.slice(trainSize),
+      testFeatures: extractedData.slice(0, trainSize),
+      testLabels: labels.slice(0, trainSize),
+    };
+
+    return dataSet;
+  }
+
+  private extractColumns(data: (string | number)[][], columnNames: string[]) {
+    const headers = _.first(data);
+
+    let extracted: (string | number)[][] = [];
+
+    if (headers) {
+      const indexes = _.map(columnNames, (column) => headers.indexOf(column));
+      extracted = _.map(data, (row) => _.pullAt(row, indexes));
+    }
+
+    return extracted;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hi Enrico');
+import { CSVManager, Options, DataSet, DataModel } from './CSVManager';
+
+function sqFoots2sqMetres(sqrareFoot: string | number): number {
+  return sqrareFoot * 0.092903;
+}
+
+const options: Options = {
+  dataColumns: ['lat', 'long', 'sqft_living'],
+  labelColumns: ['price'],
+  converters: {
+    sqft_living: (sqft_living: string | number) => {
+      return sqFoots2sqMetres(sqft_living);
+    },
+  },
+  shuffle: false,
+  splitTest: 0.1,
+};
+
+const csvManager = new CSVManager('example.csv', options);
+const dataSet = csvManager.loadCSV();
+console.log(dataSet.testLabels);
