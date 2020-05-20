3bc7237e740318b70e7fe38be980f0ba79abcddf
enrico <ezanardo@onezerobinary.com>
2020-05-20 12:34:00 +0200

    clean the code and compile the package

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,1 +1,1 @@
-export {};
+export * from './CSVManager';
--- build/index.js
+++ build/index.js
@@ -1,27 +1,13 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const CSVManager_1 = require("./CSVManager");
-// Converters
-function sqFoots2sqMetres(sqrareFoot) {
-    if (typeof sqrareFoot !== 'number') {
-        sqrareFoot = parseFloat(sqrareFoot.replace('"', ''));
-    }
-    const squareMeters = sqrareFoot * 0.092903;
-    return squareMeters;
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
 }
-// Options
-const options = {
-    dataColumns: ['lat', 'long', 'sqft_living'],
-    labelColumns: ['price'],
-    converters: {
-        sqft_living: (sqft_living) => {
-            return sqFoots2sqMetres(sqft_living);
-        },
-    },
-    shuffle: false,
-    splitTest: 0.1,
-};
-// Data Management
-const csvManager = new CSVManager_1.CSVManager('../csv/example.csv', options);
-const dataSet = csvManager.loadCSV();
-// console.log(dataSet.testLabels);
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./CSVManager"), exports);
